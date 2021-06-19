bf05b109ce843dc8bff4d47a3fc134951b62fbe1
German Firsov <firsov.german@outlook.com>
2021-06-20 01:38:51 +0200

    Test limits consideration when generating width

---
+++ hsm-api-test/Domain/DimensionGenerators/PseudoRandomWidthGenerator_GetRandomWidth.cs
@@ -0,0 +1,26 @@
+﻿using hsm_api.Domain.DimensionGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace hsm_api_test.Domain.DimensionGenerators
+{
+    public class PseudoRandomWidthGenerator_GetRandomWidth
+    {
+        [Theory]
+        [InlineData(1000, 2000)]
+        [InlineData(1000, 1500)]
+        [InlineData(1500, 2000)]
+        [InlineData(1000, 1002)]
+        public void GetRandomWidth_Limits_Considered(float lowLimit, float highLimit)
+        {
+            var generator = new PseudoRandomWidthGenerator(lowLimit, highLimit);
+            float randomWidth = generator.GetRandomWidth();
+            Assert.True(randomWidth > lowLimit, "Random width should be higher than low limit");
+            Assert.True(randomWidth < lowLimit, "Random width should be smaller than high limit");
+        }
+    }
+}
