8bb1e8dcc3333078f2cbcbb13080c6b4e3a0a4d5
German Firsov <firsov.german@outlook.com>
2021-06-20 19:43:59 +0200

    Add tests for FormatMessageName method

---
+++ hsm-api-test/Infrastructure/MessageService_FormatMessageName.cs
@@ -0,0 +1,30 @@
+﻿using hsm_api.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace hsm_api_test.Infrastructure
+{
+    public class MessageService_FormatMessageName
+    {
+        [Fact]
+        public void FormatMessageName_Use_Uppercase()
+        {
+            var formatedName = MessageService.FormatMessageName("STARTproduction");
+            Assert.Equal("STARTPRODUCTION", formatedName);
+        }
+
+        [Theory]
+        [InlineData("START_PRODUCTION")]
+        [InlineData("START__PRODUCTION")]
+        [InlineData("START$PRODUCTION")]
+        public void FormatMessageName_Remove_Non_Alphabetic(string unformatedName)
+        {
+            var formatedName = MessageService.FormatMessageName(unformatedName);
+            Assert.Equal("STARTPRODUCTION", formatedName);
+        }
+    }
+}
