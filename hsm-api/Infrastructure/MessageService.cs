004d369f69d77ca165a0d86d43a550a2f9744556
German Firsov <firsov.german@outlook.com>
2021-06-20 19:31:21 +0200

    Add unimplemented message service

---
+++ hsm-api/Infrastructure/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hsm_api.Infrastructure
+{
+    public class MessageService
+    {
+        public void IsValidMessageName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatMessageName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
