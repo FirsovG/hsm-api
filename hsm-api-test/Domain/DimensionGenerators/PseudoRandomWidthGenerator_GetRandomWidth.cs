90ec10110a0f5575714d7e9de854d621862d5363
German Firsov <firsov.german@outlook.com>
2021-06-20 15:43:32 +0200

    Test dependency of pseudo randomizer on seed

--- hsm-api-test/Domain/DimensionGenerators/PseudoRandomWidthGenerator_GetRandomWidth.cs
+++ hsm-api-test/Domain/DimensionGenerators/PseudoRandomWidthGenerator_GetRandomWidth.cs
@@ -30,9 +30,31 @@ namespace hsm_api_test.Domain.DimensionGenerators
         public void GetRandomWidth_Not_Accept_Negative_Limits(float lowLimit, float highLimit)
         {
             var generator = new PseudoRandomWidthGenerator(lowLimit, highLimit);
+
             var exception = Assert.Throws<ArgumentException>(() => generator.GetRandomWidth());
             Assert.Contains("negative", exception.Message, StringComparison.InvariantCultureIgnoreCase);
             Assert.Contains("limit", exception.Message, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void GetRandomWidth_Randomizer_Depend_On_Seed()
+        {
+            const int seed = 5;
+            const int lowLimit = 500;
+            const int highLimit = 1000;
+            var randomizer1 = new Random(seed);
+            var generator1 = new PseudoRandomWidthGenerator(randomizer1, lowLimit, highLimit);
+            var generatedWidth1 = new float[10];
+            var randomizer2 = new Random(seed);
+            var generator2 = new PseudoRandomWidthGenerator(randomizer2, lowLimit, highLimit);
+            var generatedWidth2 = new float[10];
+
+            for (int i = 0; i < generatedWidth1.Length; i++)
+                generatedWidth1[i] = generator1.GetRandomWidth();
+            for (int i = 0; i < generatedWidth2.Length; i++)
+                generatedWidth2[i] = generator2.GetRandomWidth();
+
+            Assert.Equal(generatedWidth1, generatedWidth2);
         }
     }
 }
