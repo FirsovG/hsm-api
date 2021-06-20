64449679af3080e24e6edf72b220f64fcb5fec55
German Firsov <firsov.german@outlook.com>
2021-06-20 20:03:44 +0200

    Add tests for IsKnownMessageName method

---
+++ hsm-api-test/Infrastructure/MessageService_IsKnownMessageName.cs
@@ -0,0 +1,30 @@
+﻿using hsm_api.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace hsm_api_test.Infrastructure
+{
+    public class MessageService_IsKnownMessageName
+    {
+        [Fact]
+        public void IsKnownMessageName_Throw_Exception_On_Bad_Format_With_Message()
+        {
+            var expectedMessage = $"Invalid message name format, use {nameof(MessageService.FormatMessageName)} before";
+            Action methodCall = () => MessageService.IsKnownMessageName("START$PRODUCTION");
+            
+            var exception = Assert.Throws<FormatException>(methodCall);
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void IsKnownMessageName_Start_Production_Is_Known()
+        {
+            var isKnown = MessageService.IsKnownMessageName("STARTPRODUCTION");
+            Assert.True(isKnown, "Start production should be known");
+        }
+    }
+}
