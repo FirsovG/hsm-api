1189b01368462e8e9fb4c021a5fd6497d13bc9f9
German Firsov <firsov.german@outlook.com>
2021-06-22 20:56:48 +0200

    Add not implemented coil id json converter

--- hsm-api/Models/Messages/StartProductionMessage.cs
+++ hsm-api/Models/Messages/StartProductionMessage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hsm_api.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,6 +12,7 @@ namespace hsm_api.Models.Messages
     public class StartProductionMessage : Message
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonConverter(typeof(CoilIdFormatJsonConverter))]
         public string CoilId { get; set; }
         public DateTime ProductionStartDate { get; set; } = DateTime.Now.AddSeconds(-95);
     }
