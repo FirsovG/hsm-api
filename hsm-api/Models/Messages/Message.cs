6e10bee61e17b98068d2c17f61f741830fc36c99
German Firsov <firsov.german@outlook.com>
2021-06-22 20:49:58 +0200

    Mark MessageId as Key

--- hsm-api/Models/Messages/Message.cs
+++ hsm-api/Models/Messages/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace hsm_api.Models.Messages
 {
     public abstract class Message
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long MessageId { get; set; }
         public DateTime MessageCreationDate { get; set; } = DateTime.Now;
