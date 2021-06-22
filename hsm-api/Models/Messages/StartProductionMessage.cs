daed42ba41d4245dc8437762ecf18e44f5ef771c
German Firsov <firsov.german@outlook.com>
2021-06-22 20:52:33 +0200

    Add default values for message attributes

--- hsm-api/Models/Messages/StartProductionMessage.cs
+++ hsm-api/Models/Messages/StartProductionMessage.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace hsm_api.Models.Messages
 {
     public class StartProductionMessage : Message
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string CoilId { get; set; }
-        public DateTime ProductionStartDate { get; set; }
+        public DateTime ProductionStartDate { get; set; } = DateTime.Now.AddSeconds(-95);
     }
 }
