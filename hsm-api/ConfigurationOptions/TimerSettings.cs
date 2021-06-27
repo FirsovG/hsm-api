0fd3a18751abd54a98348cd89f3f2ab836f7b353
German Firsov <firsov.german@outlook.com>
2021-06-27 21:08:22 +0200

    Add interval settings for production timers

---
+++ hsm-api/ConfigurationOptions/TimerSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hsm_api.ConfigurationOptions
+{
+    public class TimerSettings
+    {
+        public int StartProduction { get; set; }
+    }
+}
