6a22c78bc62ed3c62981511d819846cbbf93c726
German Firsov <firsov.german@outlook.com>
2021-06-28 21:59:29 +0200

    Rename and adapt the test to changes

---
+++ hsm-api-test/Infrastructure/DynamicIntervalTimer_UpdateTimerSettings.cs
@@ -0,0 +1,32 @@
+﻿using hsm_api.ConfigurationOptions;
+using hsm_api.Infrastructure;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace hsm_api_test.Infrastructure
+{
+    public class DynamicIntervalTimer_UpdateTimerSettings
+    {
+        [Fact]
+        public void UpdateTimerSettings_Resubscribes()
+        {
+            // StartProductionTimerSettings was selected rendomly
+            var longInterval = new StartProductionTimerSettings { Interval = 50000 };
+            var shortInterval = new StartProductionTimerSettings { Interval = 1 };
+            var timer = new DynamicIntervalTimer<StartProductionTimerSettings>(Mock.Of<IOptionsMonitor<StartProductionTimerSettings>>(_ => _.CurrentValue == longInterval));
+            int timeElapsedCount = 0;
+
+            timer.TimeElapsed += (_, _) => timeElapsedCount++;
+            timer.UpdateTimerSettings(shortInterval);
+            Task.Delay(10).Wait();
+
+            Assert.True(timeElapsedCount > 0, "Timer fired at least once");
+        }
+    }
+}
