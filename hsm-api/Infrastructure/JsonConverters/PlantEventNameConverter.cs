9c5554a359fca28fd9e9e5fe02efe8058327e02d
German Firsov <firsov.german@outlook.com>
2021-06-29 19:59:06 +0200

    Convert plant event name into unified format

---
+++ hsm-api/Infrastructure/JsonConverters/PlantEventNameConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace hsm_api.Infrastructure.JsonConverters
+{
+    public class PlantEventNameConverter : JsonConverter<string>
+    {
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => MessageService.FormatMessageName(reader.GetString());
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value);
+    }
+}
--- hsm-api/Models/Webhook.cs
+++ hsm-api/Models/Webhook.cs
@@ -1,6 +1,8 @@
-﻿using System.ComponentModel;
+﻿using hsm_api.Infrastructure.JsonConverters;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace hsm_api.Models
 {
@@ -9,6 +11,7 @@ namespace hsm_api.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public string CallbackUrl { get; set; }
+        [JsonConverter(typeof(PlantEventNameConverter))]
         public string SubscribedPlantEvent { get; set; }
         public bool IsActive { get; set; } = true;
     }
