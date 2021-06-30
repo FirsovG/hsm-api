d356dd681ee9239f4eacf0adb473f42b20f4b86d
German Firsov <firsov.german@outlook.com>
2021-06-30 17:23:58 +0200

    Add settings for dimension generators

---
+++ hsm-api/ConfigurationOptions/DimensionSettings/IDimensionGeneratorSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hsm_api.ConfigurationOptions.DimensionSettings
+{
+    public interface IDimensionGeneratorSettings
+    {
+        public float MinLimit { get; set; }
+        public float MaxLimit { get; set; }
+    }
+}
