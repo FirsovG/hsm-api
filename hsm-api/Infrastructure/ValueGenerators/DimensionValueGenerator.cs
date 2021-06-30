25772f8f8fabde1f9d2ae74b97508013873711a8
German Firsov <firsov.german@outlook.com>
2021-06-30 19:20:29 +0200

    Add generic dimensions generator

---
+++ hsm-api/Infrastructure/ValueGenerators/DimensionValueGenerator.cs
@@ -0,0 +1,29 @@
+﻿using hsm_api.ConfigurationOptions.DimensionSettings;
+using hsm_api.Domain.DimensionGenerators;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hsm_api.Infrastructure.ValueGenerators
+{
+    public class DimensionValueGenerator<T> : ValueGenerator<float> where T : IDimensionGeneratorSettings
+    {
+        private PseudoRandomDimensionGenerator<T> _generator;
+
+        public override bool GeneratesTemporaryValues => false;
+
+        public override float Next(EntityEntry entry)
+        {
+            // Workaround, because only parameterless constructor is called
+            // https://github.com/dotnet/efcore/issues/10792#issuecomment-560134363
+            if (_generator == null)
+                _generator = entry.Context.GetService<PseudoRandomDimensionGenerator<T>>();
+
+            return _generator.GetValue();
+        }
+    }
+}
